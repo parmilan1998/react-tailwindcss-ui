a9e6ec6a8ae15735e2eefffd1195fd71ad5c31cd
Parmilan <parmilancs2024@gmail.com>
2023-06-22 00:50:58 +0530

    Logo name, Nav links design in Appbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
   theme: {
     extend: {
         fontFamily:{
-          Cinzel:["Cinzel"]
+          Cinzel:["Cinzel"],
+          Poppins:["Poppins"]
         },
     },
   },
